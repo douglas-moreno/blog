f42a05b3f1fb6c6c4e01caddba006b2c5dc0258c
Douglas Moreno <douglas_moreno@yahoo.com.br>
2023-07-28 18:23:54 -0300

    Filamet Recource Updates

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -21,6 +21,8 @@ class CategoryResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
 
+    protected static ?string $navigationGroup = 'Content';
+
     public static function form(Form $form): Form
     {
         return $form
@@ -45,11 +47,8 @@ class CategoryResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('title'),
-                Tables\Columns\TextColumn::make('slug'),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime(),
                 Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime(),
+                    ->dateTime('d/m/y'),
             ])
             ->filters([
                 //
