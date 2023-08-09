503d1905f49c978675fdc18924d96e34b41acedc
Douglas Moreno <douglas_moreno@yahoo.com.br>
2023-08-09 16:06:10 -0300

    Category Tags fixed

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -7,11 +7,14 @@
                 <img src="{{ $post->getThumbnail() }}">
             </a>
             <div class="flex flex-col justify-start p-6 bg-white">
-                @foreach ($post->categories as $category)
-                    <a href="#" class="pb-4 text-sm font-bold text-blue-700 uppercase">
-                        {{ $category->title }}
-                    </a>
-                @endforeach
+                <div class="flex gap-4">
+                    @foreach ($post->categories as $category)
+                        <a href="{{ route('by-category', $category) }}"
+                            class="pb-4 text-sm font-bold text-blue-700 uppercase">
+                            {{ $category->title }}
+                        </a>
+                    @endforeach
+                </div>
 
                 <h1 class="pb-4 text-3xl font-bold hover:text-gray-700">
                     {{ $post->title }}
